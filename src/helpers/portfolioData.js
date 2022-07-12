85fcb584168a8dc1934ae94fc89314005f0b04e5
Jameson Wang <jamesonw10@gmail.com>
2022-07-12 17:21:12 -0400

    add guess the number game

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -1,4 +1,5 @@
 import fightingGame from "../images/projectsicon/fighting-game.png";
+import guessTheNumberGame from "../images/projectsicon/guess-the-number-game.png";
 
 // Latest project go on top
 
@@ -8,6 +9,13 @@ const portfolioData = [
         link: "https://basic-fighting-game.netlify.app/",
         title: "Fighting game",
         summary: "A mini fighting game between two rivals",
+        tech: "JavaScript | HTML | CSS",
+    },
+    {
+        image: guessTheNumberGame,
+        link: "https://guessing-the-numbers-game.netlify.app/",
+        title: "Guess My Number Game",
+        summary: "Can you guess the mystery number between 1 through 20",
         tech: "JavaScript | HTML | CSS",
     },
 ]
