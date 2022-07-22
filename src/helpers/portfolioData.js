73f8047439999b484081cb04e709bd43c2934117
Jameson Wang <jamesonw10@gmail.com>
2022-07-22 00:55:19 -0400

    add pig game

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -1,14 +1,13 @@
 import fightingGame from "../images/projectsicon/fighting-game.png";
 import guessTheNumberGame from "../images/projectsicon/guess-the-number-game.png";
-
-// Latest project go on top
+import pigGame from "../images/projectsicon/pig-game.png";
 
 const portfolioData = [
     {
-        image: fightingGame,
-        link: "https://basic-fighting-game.netlify.app/",
-        title: "Fighting game",
-        summary: "A mini fighting game between two rivals",
+        image: pigGame,
+        link: "https://friendly-pig-game.netlify.app/",
+        title: "Pig Game",
+        summary: "First player reach to 100 points WINS! But be wary of rolling a 1...it's all about chance",
         tech: "JavaScript | HTML | CSS",
     },
     {
@@ -16,6 +15,13 @@ const portfolioData = [
         link: "https://guessing-the-numbers-game.netlify.app/",
         title: "Guess My Number Game",
         summary: "Can you guess the mystery number between 1 through 20",
+        tech: "JavaScript | HTML | CSS",
+    },
+    {
+        image: fightingGame,
+        link: "https://basic-fighting-game.netlify.app/",
+        title: "Fighting game",
+        summary: "A mini fighting game between two rivals",
         tech: "JavaScript | HTML | CSS",
     },
 ]
--- src/helpers/techs.js
+++ src/helpers/techs.js
@@ -1,32 +1,32 @@
 export const techs = [
-  {
-      name: 'React',
-      iconName: 'react'
-  },
-  {
-      name: 'JavaScript',
-      iconName: 'javascript'
-  },
-  {
-      name: 'Mongo DB',
-      iconName: 'mongodb'
-  },
-  
-  {
-      name: 'Bootstrap',
-      iconName: 'bootstrap'
-  },
-  
-  {
-      name: 'GitHub',
-      iconName: 'github'
-  },
-  {
-      name: 'HTML 5',
-      iconName: 'html5'
-  },
-  {
-      name: 'CSS3',
-      iconName: 'css3'
-  },
+    {
+        name: 'React',
+        iconName: 'react'
+    },
+    {
+        name: 'JavaScript',
+        iconName: 'javascript'
+    },
+    {
+        name: 'Mongo DB',
+        iconName: 'mongodb'
+    },
+
+    {
+        name: 'Bootstrap',
+        iconName: 'bootstrap'
+    },
+
+    {
+        name: 'GitHub',
+        iconName: 'github'
+    },
+    {
+        name: 'HTML 5',
+        iconName: 'html5'
+    },
+    {
+        name: 'CSS3',
+        iconName: 'css3'
+    },
 ]
