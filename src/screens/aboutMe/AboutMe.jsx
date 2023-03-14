f41c0c2a09046ac3950da19e157f42ea4fc89232
Jameson Wang <jamesonw10@gmail.com>
2023-03-14 16:33:51 -0400

    updated aboutme

--- src/screens/aboutMe/AboutMe.jsx
+++ src/screens/aboutMe/AboutMe.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import './aboutMe.css';
-// import Profile from "../../images/profile.jpeg";
+import Profile from '../../images/profile.jpeg';
 
 function AboutMe() {
   return (
     <div className="about_container" id="about-me">
       <div className="profile-pic">
-        {/* <img src={Profile} alt="person icon" /> */}
+        <img src={Profile} alt="person icon" />
       </div>
       <div className="about_text">
         <h1>ABOUT ME</h1>
         <p>
-          I aim to leverage my technical expertise and creativity to develop
-          innovative software solutions that make everyday life easier. One of
-          my objectives is to continuously learn new developing technologies and
-          programming languages, while also improving my problem-solving skills.
+          Hello there! I'm passionate about leveraging my technical expertise
+          and creativity to develop innovative software solutions that make
+          everyday life easier. One of my top goals is to continually learn new
+          technologies and programming languages, while also honing my
+          problem-solving skills. When I'm not working on software projects, I
+          enjoy practicing archery and meeting random strangers on the street in
+          my city to socialize!
         </p>
       </div>
     </div>
